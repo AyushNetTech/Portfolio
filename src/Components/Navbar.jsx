2e2bff058c912aa39c7a72e479c394a45f2725d6
ayush1 <ayushsunilupase999@gmail.com>
2025-07-17 21:58:59 +0530

    first

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import "./Navbar.css"
+
+const Navbar = ()=>{
+    return(
+        <div className="Navbar">
+                <div className="l">HOME</div>
+                <div className="l">PROJECTS</div>
+                <div className="l">SKILLS</div>
+                <div className="l">CONTACT</div>
+        </div>
+    )
+
+}
+
+export default Navbar;
