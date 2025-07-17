9f6eaebea23e8d1641bdcb325bbe5f95ec453a13
ayush1 <ayushsunilupase999@gmail.com>
2025-07-17 22:37:08 +0530

    hii

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = ()=>{
                 <div className="l">HOME</div>
                 <div className="l">PROJECTS</div>
                 <div className="l">SKILLS</div>
-                <div className="l">CONTACT</div>
+                <div className="Contact l">CONTACT</div>
         </div>
     )
 
