6d8a87276cb227144d39a49d0c26490e72db7188
ayush1 <ayushsunilupase999@gmail.com>
2025-07-20 17:34:09 +0530

    add hello

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -4,7 +4,13 @@ import "./Intro.css"
 const Intro = ()=>{
     return (
         <div className="Intro">
-            
+            <div className="Hello">
+                <span>H</span>
+                <span>E</span>
+                <span>L</span>
+                <span>L</span>
+                <span>O</span>
+            </div>
         </div>
     )
 }
