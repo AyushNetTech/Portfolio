5fa6b7dc5c3bd481eae94ef16dfd84323c1a1a9b
ayush1 <ayushsunilupase999@gmail.com>
2025-07-20 23:34:22 +0530

    add animations on text

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -1,15 +1,29 @@
 import React from "react"
 import "./Intro.css"
+import { useGSAP } from "@gsap/react"
+import gsap from 'gsap';
+import Navbar from "./Navbar";
+import Projects from "./Projects";
+
 
 const Intro = ()=>{
+
+    useGSAP(()=>{
+        gsap.from(".text",{
+            y:300,
+            duration:1,
+            stagger:0.07,
+        })
+    })
     return (
         <div className="Intro">
+            {/* <Navbar/> */}
             <div className="Hello">
-                <span>H</span>
-                <span>E</span>
-                <span>L</span>
-                <span>L</span>
-                <span>O</span>
+                <span className="text">H</span>
+                <span className="text">E</span>
+                <span className="text">L</span>
+                <span className="text">L</span>
+                <span className="text">O</span>
             </div>
         </div>
     )
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,19 @@
 import React from "react"
 import "./Navbar.css"
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
 
 const Navbar = ()=>{
+
+    useGSAP(()=>{
+        gsap.from(".Navbar",{
+            y:-100,
+            delay:1,
+            opacity:0,
+            duration:1,
+        })
+    })
+
     return(
         <div className="Navbar">
                 <div className="l">HOME</div>
