5d9a9ae255fe5904440f03062a6797e789ce182a
ayush1 <ayushsunilupase999@gmail.com>
2025-07-23 00:06:58 +0530

    add some in info page

--- src/Components/Info.jsx
+++ src/Components/Info.jsx
@@ -4,7 +4,11 @@ import "./Info.css"
 const Info=()=>{
     return(
         <div className="Info">
-
+            <span id="a">I Am</span>
+            <span id="b">Ayush Upase</span><br></br>
+            <span id="c">Full Stack Developer</span><br></br>
+            <span id="d">and</span>
+            <span id="e">Android Developer</span>
         </div>
     )
 }
