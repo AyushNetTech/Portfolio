cdd1ca2cd273e416b9c7f684f5b68f07629c3684
ayush1 <ayushsunilupase999@gmail.com>
2025-07-25 22:27:25 +0530

    animation done

--- src/Components/Info.jsx
+++ src/Components/Info.jsx
@@ -1,25 +1,92 @@
 import React from "react"
 import "./Info.css"
 import infoV from "./video.mp4"
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger); // ✅ Add this at the top
 
 const Info=()=>{
+
+    useGSAP(() => {
+        gsap.from("#a", {
+            x: -300,
+            duration: 0.5,
+            scrollTrigger: {
+                trigger: ".Intro",
+                start: "top 60%",
+            },
+        });
+        gsap.from("#b", {
+            y: 200,
+            delay:0.7,
+            duration: 0.5,
+            scrollTrigger: {
+                trigger: ".Intro",
+                start: "top 60%",
+            },
+        });
+         gsap.from("#c", {
+            x: 500,
+            delay:0.7,
+            duration: 0.5,
+            scrollTrigger: {
+                trigger: ".Intro",
+                start: "top 30%",
+            },
+        });
+        gsap.to("#d", {
+            scale: 1,
+            delay:0.7,
+            duration: 0.5,
+            scrollTrigger: {
+                trigger: ".Intro",
+                start: "top 30%",
+            },
+        });
+        gsap.from("#e", {
+            y:200,
+            delay:0.7,
+            duration: 0.5,
+            scrollTrigger: {
+                trigger: ".Intro",
+                start: "top 30%",
+            },
+        });
+        gsap.from(".VFrame", {
+            x:-600,
+            delay:1,
+            duration: 0.5,
+            scrollTrigger: {
+                trigger: ".Intro",
+                start: "top 30%",
+            },
+        });
+    });
+
     return(
-        <div className="Upper">
-            <div className="ab">
-                <span id="a">I Am</span>
-                <span id="b">Ayush Upase</span><br></br>
+        <div className="Info">
+            <div className="Upper">
+                <div className="divA">
+                    <h1 id="a">I Am</h1>
+                </div>
+                <div className="divB">
+                    <h1 id="b">Ayush Upase</h1>
+                </div>
             </div>
             <div className="Below">
                 <div className="Text">
                     <div className="TextA">
                         <div className="Text1">
-                            <span id="c">Full Stack Developer</span>
+                            <h1 id="c">Full Stack Developer</h1>
                         </div>
                         <div className="Text2">
-                            <span id="d">AND</span>
+                            <h1 id="d">AND</h1>
                         </div>
                     </div>
-                    <span id="e">Android Developer</span>
+                    <div className="TextB">
+                        <h1 id="e">Android Developer</h1>
+                    </div>
                 </div>
                 <div className="Video">
                     <div className="VFrame">
