f2088c5a7880ebb87de74cf6054caadc846db80f
ayush1 <ayushsunilupase999@gmail.com>
2025-07-31 23:22:41 +0530

    only Info Marker

--- src/Components/Info.jsx
+++ src/Components/Info.jsx
@@ -30,7 +30,7 @@ const Info = () => {
             const tlDesktop = gsap.timeline({
                 scrollTrigger: {
                     trigger: ".Info",
-                    start: "top 80%",
+                    start: "top 50%",
                     markers: true
                 },
                 defaults: { duration: 0.5, ease: "power2.out" },
